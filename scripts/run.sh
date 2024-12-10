64dc08261c52efe10dca59acdb447807bfd42c27
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-12-10 09:34:10 +0100

    fix: run

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 day=$1
 
+
 if [[ $day == "" ]]; then
     echo "Provide a christmas date"
     exit 1
 fi
 
-gcc ./src/2024/"$1".c -o ./output/"$1" && ./output/"$1"
+# -g is with debug information
+gcc -g ./src/2024/"$day".c -o ./output/"$day" && [ "$2" == "-g" ] && gdb -q ./output/"$day" || ./output/"$day"
