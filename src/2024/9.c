95acba0502cd628a4ac4f2915ad6953384aa91a9
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-12-10 10:38:01 +0100

    test: unpack

--- src/2024/9.c
+++ src/2024/9.c
@@ -4,10 +4,40 @@
 //https://adventofcode.com/2024/day/9
 
 //We add +2 to the id:s
-void unpack_disc_map(char *map, int* unpacked){
-    unpacked[0] = 0;
-    unpacked[1] = -1;
-    unpacked[2] = -1;
+int unpack_disc_map(char *map, int* unpacked){
+    
+    int next = 0;
+    int STATE_disc = 1;
+    // int id = 0;
+    int unpacked_i = 0;
+    int i = 0;
+    while (map[i] != 0)
+    {
+        next = map[i] - 0x30;
+        if(STATE_disc){
+            
+            for (int i_state = 0; i_state < next; i_state++)
+            {
+                unpacked[unpacked_i] = 0;
+                unpacked_i++;
+            }
+
+            STATE_disc = 0;
+        }
+        //STATE disc space
+        else {
+            for (int i_state = 0; i_state < next; i_state++)
+            {
+                unpacked[unpacked_i] = -1;
+                unpacked_i++;
+            }
+            
+            STATE_disc = 1;   
+        }
+
+        i++;
+    }
+    return i;
 }
 
 //--------------Test helpers------------------
@@ -58,20 +88,44 @@ void log_array_result(int *exp, int *res, int equals, int len, char *test_name){
 
 //--------------Test code--------------------
 
-void unpack_disc_map__12__0m1m1() //translate numbs by 2
+void unpack_disc_map__12__0m1m1()
 {
     char *map = "12";
-    int res[3];
-    unpack_disc_map(map, res);
+    int res[10];
+    int len = unpack_disc_map(map, res);
     int exp[3] = {0, -1, -1};
     
-    int equals = int_array_equals(exp, res, 3);
+    int equals = int_array_equals(exp, res, len);
     log_array_result(exp, res, equals, 3,  "unpack_disc_map__12__0m1m1");
+}
+
+void unpack_disc_map__11__0m1()
+{
+    char *map = "11";
+    int res[10];
+    int len = unpack_disc_map(map, res);
+    int exp[3] = {0, -1};
+    
+    int equals = int_array_equals(exp, res, len);
+    log_array_result(exp, res, equals, len,  "unpack_disc_map__11__0m1");
+}
+
+void unpack_disc_map__21__00m1()
+{
+    char *map = "21";
+    int res[10];
+    int len = unpack_disc_map(map, res);
+    int exp[3] = {0, 0, -1};
+    
+    int equals = int_array_equals(exp, res, len);
+    log_array_result(exp, res, equals, len,  "unpack_disc_map__21__00m1");
 }
 
 int main(){
 
     unpack_disc_map__12__0m1m1();
+    unpack_disc_map__11__0m1();
+    unpack_disc_map__21__00m1();
 
     //Summary
     printf("---------------------------------------\n");
